cadb17d699a30a204bbe1aaf36fd26e42ba0f926
deepikah56 <dhonnagirigowda@qwinix.io>
2018-04-10 17:14:02 +0530

    file

--- questions/3.rb
+++ questions/3.rb
@@ -15,6 +15,17 @@
 # >Hello.txt
 # Deleted file
 #        Or
+
 # File not found
-puts "1.Add file"
-puts "2.Delete file"
+puts "Enter the filename" 
+file=gets.to_s 
+aFile=File.new(file, "w") 
+puts "Enter the text" 
+info=gets.to_s 
+aFile.puts(info) 
+puts "New file is created" 
+aFile.close 
+puts "Enter the delete filename" 
+h=gets.to_s 
+File.delete(h) 
+puts"file deleted"
